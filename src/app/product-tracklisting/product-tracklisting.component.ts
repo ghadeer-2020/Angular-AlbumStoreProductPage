9cb768c0e9507c16e207b49e2771a8554f5548d0
ghadeer-2020 <ghadir2013@gmail.com>
2020-06-29 19:24:55 +0300

    Completed module

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductTracklistingComponent implements OnInit {
  albumInfo: Album;
-  constructor() { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
      this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
